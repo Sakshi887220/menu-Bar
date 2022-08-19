7bcfa2ec29c89c6fb055ac9a978c7f2d10e5499a
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-08-19 09:30:26 +0530

    Update Notebook.py

--- Notebook.py
+++ Notebook.py
@@ -8,22 +8,14 @@ from tkinter.ttk import *
 root = Tk() 
 root.title('Menu Demonstration') 
 root.geometry("500x500")
-menubar = Menu(root)
-root.config(menu = menubar) 
+
 # Creating Menubar 
  
 def newfile():
     a = Label(root , text = "You clicked on new file!").pack()
-def openfile():
-    a = Label(root , text = "You clicked on open file!").pack()
-def savefile():
-    a = Label(root , text = "You clicked on save file!").pack()
-def cutfile():
-    a = Label(root , text = "You clicked on cut").pack()
-def copyfile():
-    a = Label(root , text = "You clicked on copy").pack()
-def pastefile():
-    a = Label(root , text = "You clicked on paste").pack()
+
+  
+  
 # Adding File Menu and commands 
 file = Menu(menubar, tearoff = 0) 
 menubar.add_cascade(label ='File', menu = file) 
@@ -45,12 +37,7 @@ edit.add_command(label ='Find...', command = None)
 edit.add_command(label ='Find again', command = None) 
   
 # Adding Help Menu 
-help_ = Menu(menubar, tearoff = 0) 
-menubar.add_cascade(label ='Help', menu = help_) 
-help_.add_command(label ='Tk Help', command = None) 
-help_.add_command(label ='Demo', command = None) 
-help_.add_separator() 
-help_.add_command(label ='About Tk', command = None) 
+
   
 # display Menu 
 
